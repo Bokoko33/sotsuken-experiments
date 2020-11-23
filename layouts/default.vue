7fcbe6393a41b90bea78b57f8cdc04dc4affc1da
Bokoko <kamabokoman1042@gmail.com>
2020-11-23 16:57:48 +0900

    開始/終了ページでは通常操作

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="layout">
     <Nuxt />
-    <InterFace />
+    <InterFace v-show="showInterFace" />
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    showInterFace() {
+      return this.$route.name !== 'index' && this.$route.name !== 'finished';
+    },
+  },
+};
+</script>
