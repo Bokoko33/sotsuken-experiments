e5eba3963c12e3cab163a87f1112bf3c09443fdb
Bokoko <kamabokoman1042@gmail.com>
2020-11-23 16:59:16 +0900

    自由記述のアンケート追加、送信する結果を見やすく

--- pages/finished.vue
+++ pages/finished.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="finished">
-    <p class="finished__text">
-      実験は以上になります。<br />
-      {{ stateText }}
+    <h1 class="finised__heading">最後に記述でのアンケートにお答えください。</h1>
+    <p class="finished__quest">
+      実験中、「通常のカーソルと動きが違う」と感じたものについて、具体的にどう違うと感じましたか。主観で良いので自由にお答えください。
     </p>
+    <form class="finished__form" @submit.prevent="submit">
+      <textarea v-model="comment" class="finished__textarea"></textarea>
+      <input
+        type="submit"
+        class="finished__button"
+        value="送信"
+        :class="{ disabled: !buttonActive }"
+      />
+    </form>
+    <div v-if="visibleFinishedModal" class="finished__modal">
+      <p class="finished__modal-text">
+        実験は以上になります。ご協力いただきありがとうございました。
+      </p>
+    </div>
   </div>
 </template>
 
@@ -13,18 +27,32 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      ansewr: '',
-      stateText: '回答を送信中です。そのまましばらくお待ちください。',
+      comment: '',
+      buttonActive: false,
+      visibleFinishedModal: false,
     };
   },
-  mounted() {
-    this.ansewr = `パターン: ${this.$interFace.forcePattern}, 回答: ${this.$store.state.questAnswer}`;
-    this.submit();
+  watch: {
+    comment() {
+      if (this.comment !== '' && this.comment.match(/\S/g)) {
+        this.buttonActive = true;
+      } else {
+        this.buttonActive = false;
+      }
+    },
   },
   methods: {
     submit() {
+      if (!this.buttonActive) return;
+
       const submitParams = new FormData();
-      submitParams.append('entry.757631055', this.ansewr);
+
+      const answer = `抵抗力提示パターン : ${this.$interFace.forcePattern}
+                      普段のポインター速度 : ${this.$store.state.defaultMouseSpeed}
+                      タスクごとの回答 : ${this.$store.state.questAnswer}
+                      最後のコメント : ${this.comment}`;
+
+      submitParams.append('entry.972973618', answer);
 
       // CROS対策
       const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
@@ -34,11 +62,12 @@ export default {
 
       const url = CORS_PROXY + formAction;
 
+      this.buttonActive = false; // 複数回押せないように
+
       axios
         .post(url, submitParams)
         .then((res) => {
-          this.stateText =
-            '回答の送信が完了しました。ブラウザを閉じて終了してください。';
+          this.visibleFinishedModal = true;
         })
         .catch((error) => {
           if (error) alert('送信できませんでした。');
@@ -52,13 +81,55 @@ export default {
 .finished {
   width: 100vw;
   height: 100vh;
+  padding: 15vh 15vw;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  cursor: auto;
+}
+
+.finished__heading {
+  text-align: center;
+}
+
+.finished__quest {
+  font-size: 18px;
+  margin-top: 60px;
+}
+
+.finished__form {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.finished__textarea {
+  width: 100%;
+  height: 120px;
+}
+
+.finished__button {
+  margin-top: 40px;
+  width: 60px;
+  height: 40px;
+  color: white;
+  background-color: orange;
+}
+
+.disabled {
+  opacity: 0.5;
+}
+
+.finished__modal {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
   display: flex;
   align-items: center;
   justify-content: center;
-}
-
-.finished__text {
-  font-size: 20px;
-  text-align: center;
+  background-color: white;
 }
 </style>
