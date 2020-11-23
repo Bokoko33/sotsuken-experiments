1161313cf46701b2cf1789c42c3160b5ad5744c7
Bokoko <kamabokoman1042@gmail.com>
2020-11-23 17:51:06 +0900

    説明の追記、pc/spの表示わけ

--- pages/index.vue
+++ pages/index.vue
@@ -2,44 +2,102 @@
   <div class="intro">
     <h1 class="intro__heading">実験についての説明</h1>
     <p class="intro__note">全て読んでから次に進んでください。</p>
-    <div class="intro__desc">
+    <div class="intro__desc-pc">
       <p class="intro__text">
         この実験サイトでは、動きを遅延したカーソルを操作していただき、抵抗感を感じたかどうかの検証を行います。<br />
         遅延したカーソルと通常状態のカーソルの操作感を何度か比較していただき、その都度アンケートに答えていただきます。<br />
-        所要時間は5分〜10分程度です。時間の計測は特に行っていませんので、あまり実験を意識せず普段通りデバイスをお使いください。
+        所要時間は<strong class="intro__strong">5分〜10分</strong
+        >程度です。時間の計測は特に行っていませんので、あまり実験を意識せず普段通り操作してください。
       </p>
       <p class="intro__text">
-        PCをお使いの方は、
-        <strong class="intro__strong">ブラウザのウィンドウを最大化</strong
-        >してください。
+        本実験にご参加の前に以下の設定をお願いいたします。
       </p>
-      <!-- <ol class="intro__list">
+      <ol class="intro__list">
         <li class="intro__list-item">
-          お使いのマウス、またはトラックパッドの感度を高く設定されている場合は、
+          ・お使いのマウスまたはトラックパッドの<strong class="intro__strong"
+            >ポインターの速さ</strong
+          >をご確認ください。
+          <p class="intro__list-item-note">
+            左上のりんごマーク → システム環境設定 →
+            マウスまたはトラックパッド（お使いの環境に合わせて選択してください）→
+            軌跡の速さ
+          </p>
         </li>
         <li class="intro__list-item">
-          ブラウザのウィンドウを最大化してください
+          ・現在のポインターの速さを<strong class="intro__strong"
+            >一番遅い方を「1」とした場合</strong
+          >の数字で入力してください →
+          <select v-model="selectedNum" class="intro__list-item-select">
+            <option value="">選択して下さい</option>
+            <option v-for="i in mouseSpeeds" :key="i" :value="i">
+              {{ i }}
+            </option>
+          </select>
         </li>
-      </ol> -->
+        <li class="intro__list-item">
+          ・ポインターの速さを
+          <strong class="intro__strong">遅い方から3番目</strong>
+          に設定し直してください。
+        </li>
+        <li class="intro__list-item">
+          ・<strong class="intro__strong">ブラウザのウィンドウを最大化</strong
+          >してください
+        </li>
+      </ol>
     </div>
-    <div class="intro__link">
-      <ClickLink
-        :click-item-length="0"
-        :current-num="1"
-        :next-path="'normal'"
-      />
+    <div class="intro__desc-sp">
+      <p class="intro__text">
+        この実験サイトでは、スマートフォンから操作する場合にもマウスカーソルを用います。次のページから、画面右下に仮想のトラックパッドが出現するので、そこから操作してください。<br />
+        本実験では、動きを遅延したカーソルを操作していただき、抵抗感を感じたかどうかの検証を行います。<br />
+        遅延したカーソルと通常状態のカーソルの操作感を何度か比較していただき、その都度アンケートに答えていただきます。<br />
+        所要時間は<strong class="intro__strong">5分〜10分</strong
+        >程度です。時間の計測は特に行っていませんので、あまり実験を意識せず普段通り操作してください。
+      </p>
     </div>
+    <div to="/normal" class="intro__link" @click="clicked">始める</div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      mouseSpeeds: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      selectedNum: '',
+    };
+  },
+  methods: {
+    clicked() {
+      if (window.innerWidth > this.$store.state.breakPoint) {
+        if (!this.selectedNum) {
+          alert(
+            '普段のポインターの速さを入力してください。（本実験用に設定済みで忘れてしまった場合は目安で構いません）'
+          );
+          return;
+        }
+        this.$store.commit('setDefaultMouseSpeed', this.selectedNum);
+      }
+      this.$router.push('normal');
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 .intro {
   width: 100vw;
   height: 100vh;
-  padding: 100px 10vw 0;
+  padding: 8vh 10vw 0;
   line-height: 1.6;
+  cursor: auto;
   @include device-touch {
     padding-top: 40px;
+  }
+}
+
+.intro__heading {
+  @include device-touch {
+    font-size: 24px;
   }
 }
 
@@ -47,8 +105,36 @@
   margin-top: 40px;
 }
 
+.intro__desc-pc {
+  display: block;
+  @include device-touch {
+    display: none;
+  }
+}
+
+.intro__desc-sp {
+  display: none;
+  @include device-touch {
+    display: block;
+  }
+}
+
 .intro__text {
-  margin-top: 20px;
+  margin-top: 40px;
+}
+
+.intro__list-item {
+  margin-top: 10px;
+}
+
+.intro__list-item-note {
+  margin-left: 20px;
+}
+
+.intro__list-item-select {
+  border: solid 1px orange;
+  color: orange;
+  border-radius: 6px;
 }
 
 .intro__strong {
@@ -56,7 +142,20 @@
 }
 
 .intro__link {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 100px;
+  height: 60px;
+  border: solid 1px orange;
+  color: orange;
+  border-radius: 6px;
+
   margin: 100px auto 0;
+  &:hover {
+    color: white;
+    background-color: orange;
+  }
 }
 </style>
